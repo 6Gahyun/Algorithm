441a975edecbb8b57b7b20cdbc8f22169971845a
6Gahyun <137782300+6Gahyun@users.noreply.github.com>
2023-08-19 18:19:29 +0900

    [Bronze I] Title: 유진수, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1356. 유진수/유진수.py
+++ 백준/Bronze/1356. 유진수/유진수.py
@@ -1,20 +1,17 @@
 N = input()
 res = 'NO'
 
+for k in range(1,len(N)):
+    a = N[:k]
+    b = N[k:]
 
-for i in range(len(N)-1):
-    a, b = 1, 1
-    if i+1 == len(N):
-        break
-    else:
-        a = map(int, N[:i+1])
-        b = map(int, N[i+1:])
-        c = 1
-        d = 1
-        for i in a:
-            c *= i
-        for j in b:
-            d *= j
-        if c == d:
-            res = 'YES'
+    c=1
+    d=1
+    for i in a:
+        c *= int(i)
+    for j in b:
+        d *= int(j)
+
+    if c==d:
+        res = 'YES'
 print(res)
